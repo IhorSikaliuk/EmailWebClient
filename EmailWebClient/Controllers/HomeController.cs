d16ec67520966c1c3f2fd13ba8d6ea59f23aa1cc
Ihor Sikaliuk <aw9810@hotmail.com>
2020-04-10 21:36:53 +0300

    update

--- EmailWebClient/Controllers/HomeController.cs
+++ EmailWebClient/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using EmailWebClient.Models;
 using MailKit;
 using MimeKit;
 using MailKit.Net.Imap;
+using System.Threading;
 
 namespace EmailWebClient.Controllers
 {
@@ -18,18 +19,43 @@ namespace EmailWebClient.Controllers
 
             throw new IndexOutOfRangeException();
         }
-        private MimeMessage[] GetMessages(IMailFolder folder) {
 
+        private void MailUpdate() {
+            MailFolder folder;
+            try {
+                while (Session.Count > 0)
+                {
+                    folder = (MailFolder)Session["folder"];
+                    Session["messages"] = GetMessages(folder);
+                    System.Diagnostics.Debug.WriteLine("DONE");
+                }
 
-            return new MimeMessage[0];
+            }
+            catch (System.NullReferenceException) {
+                System.Diagnostics.Debug.WriteLine("return");
+                return;
+            }
+            catch (Exception e) {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+                Session.Abandon();
+            }
         }
+        private List<MimeMessage> GetMessages(IMailFolder folder) {
+            int count = folder.Count;
+            List<MimeMessage> messages = new List<MimeMessage>();
+            for (int i = count - 1; i >= count - 11; i--)
+                messages.Add(folder.GetMessage(i));
+
+            return messages;
+        }
+
+        [HttpGet]
         public ActionResult Index() {
             ViewBag.Servers = DBContext.ServerConfig;
             if (Request.Cookies["serverId"] != null)
                 ViewBag.ServerId = Request.Cookies["serverId"].Value;
-
-
-            return View();
+                        
+            return View("Index");
         }
         [HttpPost]
         public ActionResult Index(Authentication login) {
@@ -56,18 +82,53 @@ namespace EmailWebClient.Controllers
             }
             Session["login"] = login;
             Session["connection"] = client;
+
             IMailFolder inbox = client.Inbox;
-            inbox.Open(FolderAccess.ReadWrite);
-            Session["inbox"] = inbox;
+            inbox.Open(FolderAccess.ReadOnly);
+            
+            Session["folder"] = inbox;
+            Session["messages"] = new List<MimeMessage>();
+            
+            return Index();
+        }
 
+        public ActionResult Mail(int id) {
+            //List<MimeMessage> messages = new List<MimeMessage>();
+            //if (Session["updated"] != true)
+            //{
+            /*    ImapClient client = (ImapClient)Session["client"];
+                IMailFolder inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+                messages = GetMessages((IMailFolder)Session["inbox"]);
+                Session["updated"] = true;*/
+            //}
 
-            return View("test");
+            return View("Index");
+        }
+
+        public ActionResult MailList(int page = 0) {
+            IMailFolder inbox = (IMailFolder)Session["folder"];
+            List<MimeMessage> messages = GetMessages(inbox);
+            List<string> subjects = new List<string>();
+            List<DateTimeOffset> dates = new List<DateTimeOffset>();
+            List<MailboxAddress> senders = new List<MailboxAddress>();
+            foreach (var message in messages)
+            {
+                subjects.Add(message.Subject);
+                dates.Add(message.Date);
+                senders.Add(message.Sender);
+            }
+            ViewBag.Subjects = subjects;
+            ViewBag.Dates = dates;
+            ViewBag.Senders = senders;
+            ViewBag.Count = messages.Count;
+
+            return PartialView();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-            Session.Abandon();
             
             return View();
         }
@@ -77,6 +138,13 @@ namespace EmailWebClient.Controllers
             ViewBag.Message = "Your contact page.";
             
             return View();
+        }
+
+        public ActionResult Exit() {
+            Session.Clear();
+            Session.Abandon();
+
+            return Index();
         }
     }
 }
---
+++ EmailWebClient/Scripts/sidelist.loader.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+	$("#loading").hide();
+	$("#empty").hide();
+	var page = 0
+	$(document).ready(function () {
+		$('#loading').show();
+		$.ajax({
+			type: 'GET',
+			url: '/Home/MailList',
+			success: function (data, textstasus) {
+				if (data != '' && page == 0) {
+					$("#sidelist").append(data);
+					++page;
+				}
+				else {
+					$("#empty").show();
+				}
+				$("#loading").hide();
+			},
+			error: function (data, textstasus) {
+				$("#loading").hide();
+				$("#empty").show();
+		}
+		});
+	});
+});
